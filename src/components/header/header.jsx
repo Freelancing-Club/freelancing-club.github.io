67cf3a840105883686aa2ed777fd58ddca2eeb88
marcusayan <128210160+Ayanstringer2002@users.noreply.github.com>
2024-01-20 18:49:58 +0530

    Update header.jsx

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,12 +1,1 @@
-import React from "react";
-import styled from "styled-components";
 
-const Container = styled.div`
-  background-color: blue;
-`;
-
-const Header = () => {
-  return <Container>Header</Container>;
-};
-
-export default Header;
