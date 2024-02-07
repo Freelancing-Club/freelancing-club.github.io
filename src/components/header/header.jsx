69d99ef56f9087ab75cf1c3a3d9f0d2dcff9be5b
sarfarajansari <sarfarajansaridev@gmail.com>
2024-02-07 20:48:35 +0530

    added header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,41 +1,38 @@
-import React from 'react';
+import React from "react";
+import "./header.scss";
 
+const sections = [
+  {
+    id: "home",
+    label: "Home",
+  },
+  {
+    id: "events",
+    label: "Events",
+  },
+  {
+    id: "comittee",
+    label: "Committe",
+  },
+  {
+    id: "contactUs",
+    label: "Contact Us",
+  },
+];
 const Header = () => {
   return (
-    <div className="nav">
-      <a href="/" aria-current="page" className="logo-link w-inline-block w--current">
-        <img
-          src="https://drive.google.com/file/d/1w2Te-ZZ1OkoV9jfZPAkLFvxluY1zcTje/view?usp=drive_link"
-          loading="eager"
-          alt="Club logo"
-          className="logo"
-        />
-      </a>
-      <div className="nav-list">
-        <a href="#Home" className="nav-item">
-          Home
-        </a>
-        <a href="#Events" className="nav-item">
-          Events
-        </a>
-        <a href="#Committe" className="nav-item">
-          Committe
-        </a>
-        <a href="#ContactUs" className="nav-item">
-          Contact Us
-        </a>
-      </div>
-      <div className="nav-buttons">
-        <a href="https://freelancingcollegeclub.com" className="button-secondary left w-inline-block">
-          <div className="button-text">Login</div>
-        </a>
-        <a href="/login" className="button-primary w-inline-block">
-          <div className="button-text">Login</div>
-        </a>
-      </div>
-    </div>
+    <header>
+      <nav>
+        {sections.map(({ id, label }) => {
+          return (
+            <div key={id}>
+              <a href={"#" + id}>{label}</a>
+            </div>
+          );
+        })}
+      </nav>
+    </header>
   );
-}
+};
 
 export default Header;
-
